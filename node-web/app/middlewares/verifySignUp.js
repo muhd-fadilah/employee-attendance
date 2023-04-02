8e79b59dc4185442a168a17febaa32ca8bd41df3
muhd-fadilah <mhdrfadilah@protonmail.com>
2023-04-02 11:06:10 +0700

    add secret key when sign up as admin

--- node-web/app/middlewares/verifySignUp.js
+++ node-web/app/middlewares/verifySignUp.js
@@ -3,21 +3,23 @@ const roles = db.roles;
 const employee = db.employee;
 
 checkDuplicateEin = (req, res, next) => {
-  employee.findOne({
-    ein: req.body.ein
-  }).exec()
-  .then((employee) => {
-    if(employee) {
-      res.status(400).send({ message: "Failed! EIN is already in use!" });
-      return;
-    }
+  employee
+    .findOne({
+      ein: req.body.ein,
+    })
+    .exec()
+    .then((employee) => {
+      if (employee) {
+        res.status(400).send({ message: "Failed! EIN is already in use!" });
+        return;
+      }
 
-    next();
-  })
-  .catch((err) => {
-    res.status(500).send({ message: err });
-    return;
-  });
+      next();
+    })
+    .catch((err) => {
+      res.status(500).send({ message: err });
+      return;
+    });
 };
 
 checkRolesExisted = (req, res, next) => {
@@ -25,7 +27,17 @@ checkRolesExisted = (req, res, next) => {
     for (let i = 0; i < req.body.roles.length; i++) {
       if (!roles.includes(req.body.roles[i])) {
         res.status(400).send({
-          message: `Failed! Role ${req.body.roles[i]} does not exist!`
+          message: `Failed! Role ${req.body.roles[i]} does not exist!`,
+        });
+        return;
+      }
+
+      if (
+        req.body.roles[i] == "admin" &&
+        req.body.admin_secret_key != process.env.ADMIN_SECRET_KEY
+      ) {
+        res.status(400).send({
+          message: `Failed! Admin secret key is not valid!`,
         });
         return;
       }
@@ -37,7 +49,7 @@ checkRolesExisted = (req, res, next) => {
 
 const verifySignUp = {
   checkDuplicateEin,
-  checkRolesExisted
+  checkRolesExisted,
 };
 
 module.exports = verifySignUp;
